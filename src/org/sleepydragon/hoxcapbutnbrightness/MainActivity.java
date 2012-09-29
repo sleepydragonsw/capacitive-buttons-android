4c2217ae456f5d675b6721315517799eacea54f6
Denver Coneybeare <denver@sleepydragon.org>
2012-09-29 15:23:27 -0400

    remove the menu

--- src/org/sleepydragon/hoxcapbutnbrightness/MainActivity.java
+++ src/org/sleepydragon/hoxcapbutnbrightness/MainActivity.java
@@ -2,7 +2,6 @@ package org.sleepydragon.hoxcapbutnbrightness;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 
 public class MainActivity extends Activity {
 
@@ -10,11 +9,5 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
     }
 }
