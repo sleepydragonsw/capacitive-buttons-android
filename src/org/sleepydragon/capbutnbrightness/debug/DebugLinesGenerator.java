794949a97109a67f1dab7e14cb166c96ec673779
Denver Coneybeare <denver@sleepydragon.org>
2012-12-30 17:59:32 -0500

    added file information to debug screen

--- src/org/sleepydragon/capbutnbrightness/debug/DebugLinesGenerator.java
+++ src/org/sleepydragon/capbutnbrightness/debug/DebugLinesGenerator.java
@@ -16,12 +16,18 @@
  */
 package org.sleepydragon.capbutnbrightness.debug;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.sleepydragon.capbutnbrightness.R;
+import org.sleepydragon.capbutnbrightness.debug.DebugFilesProvider.FileInfo;
+import org.sleepydragon.capbutnbrightness.devices.CapacitiveButtonsBacklightBrightness;
 import org.sleepydragon.capbutnbrightness.devices.DeviceInfo;
 
 import android.content.Context;
@@ -71,6 +77,7 @@ public class DebugLinesGenerator implements Iterable<String>, Iterator<String> {
     private Section[] getSections() {
         final List<Section> list = new ArrayList<Section>();
         list.add(new AppVersionSection());
+        list.add(new DeviceSection());
         list.add(new BuildSection());
         final Section[] array = new Section[list.size()];
         return list.toArray(array);
@@ -124,13 +131,14 @@ public class DebugLinesGenerator implements Iterable<String>, Iterator<String> {
                 this.sectionsIndex++;
             }
 
-            if (section == null) {
-                break;
-            } else {
+            if (section != null) {
                 final String[] lines = section.getLines();
                 this.curSection = lines;
                 this.curSectionIndex = 0;
+                next = ""; // put empty line in between sections
             }
+
+            break;
         }
 
         this.next = next;
@@ -154,22 +162,13 @@ public class DebugLinesGenerator implements Iterable<String>, Iterator<String> {
 
         public String[] getLines() {
             final Context context = DebugLinesGenerator.this.context;
-            final String boardValue = Build.BOARD;
-            final String board =
-                context.getString(R.string.debug_build_board, boardValue);
             final String bootloaderValue = Build.BOOTLOADER;
             final String bootloader =
                 context.getString(R.string.debug_build_bootloader,
                     bootloaderValue);
-            final String brandValue = Build.BRAND;
-            final String brand =
-                context.getString(R.string.debug_build_brand, brandValue);
             final String deviceValue = Build.DEVICE;
             final String device =
                 context.getString(R.string.debug_build_device, deviceValue);
-            final String hardwareValue = Build.HARDWARE;
-            final String hardware =
-                context.getString(R.string.debug_build_hardware, hardwareValue);
             final String displayValue = Build.DISPLAY;
             final String display =
                 context.getString(R.string.debug_build_display, displayValue);
@@ -187,8 +186,148 @@ public class DebugLinesGenerator implements Iterable<String>, Iterator<String> {
             final String version =
                 context.getString(R.string.debug_build_version_release,
                     versionValue);
-            return new String[] { board, bootloader, brand, device, display,
-                hardware, manufacturer, model, product, version };
+            return new String[] { bootloader, device, display, manufacturer,
+                model, product, version };
+        }
+
+    }
+
+    private class DeviceSection implements Section {
+
+        public String[] getLines() {
+            final Context context = DebugLinesGenerator.this.context;
+            final DeviceInfo device = DebugLinesGenerator.this.device;
+            final CapacitiveButtonsBacklightBrightness cap =
+                device.getCapacitiveButtonsBacklightBrightness();
+            final List<String> lines = new ArrayList<String>();
+
+            final String nameValue = device.getDisplayName();
+            final String name =
+                context.getString(R.string.debug_device_name, nameValue);
+            lines.add(name);
+
+            final boolean supportedValue = (cap != null);
+            final int supportedResId =
+                supportedValue ? R.string.yes : R.string.no;
+            final String supportedValueStr = context.getString(supportedResId);
+            final String supported =
+                context.getString(R.string.debug_device_supported,
+                    supportedValueStr);
+            lines.add(supported);
+
+            if (cap != null) {
+                final boolean possibleValue = cap.isSupported();
+                final int possibleResId =
+                    possibleValue ? R.string.yes : R.string.no;
+                final String possibleValueStr =
+                    context.getString(possibleResId);
+                final String possible =
+                    context.getString(R.string.debug_device_possible,
+                        possibleValueStr);
+                lines.add(possible);
+
+                if (cap instanceof DebugFilesProvider) {
+                    final DebugFilesProvider filesProvider =
+                        (DebugFilesProvider) cap;
+                    final FileInfo[] fileInfos = filesProvider.getDebugFiles();
+                    if (fileInfos != null) {
+                        for (final FileInfo fileInfo : fileInfos) {
+                            if (fileInfo == null) {
+                                continue;
+                            }
+                            final String path = fileInfo.getPath();
+                            if (path == null) {
+                                continue;
+                            }
+                            lines.add("");
+                            lines.add(path);
+
+                            final File file = new File(path);
+                            final boolean existsValue = file.exists();
+                            final int existsValueResId =
+                                existsValue ? R.string.yes : R.string.no;
+                            final String existsValueStr =
+                                context.getString(existsValueResId);
+                            final String exists =
+                                context.getString(R.string.debug_file_exists,
+                                    existsValueStr);
+                            lines.add(exists);
+
+                            if (existsValue) {
+                                final boolean canRead = file.canRead();
+                                final boolean canWrite = file.canWrite();
+
+                                final int permsResId;
+                                if (canRead && canWrite) {
+                                    permsResId = R.string.debug_file_readwrite;
+                                } else if (canRead) {
+                                    permsResId = R.string.debug_file_readonly;
+                                } else if (canWrite) {
+                                    permsResId = R.string.debug_file_writeonly;
+                                } else {
+                                    permsResId = R.string.debug_file_noperms;
+                                }
+                                final String permsValueStr =
+                                    context.getString(permsResId);
+                                final String perms =
+                                    context.getString(
+                                        R.string.debug_file_permissions,
+                                        permsValueStr);
+                                lines.add(perms);
+
+                                if (canRead) {
+                                    String fileContents = "";
+                                    InputStream fin;
+                                    try {
+                                        fin = new FileInputStream(file);
+                                    } catch (final IOException e) {
+                                        fileContents = e.toString();
+                                        fin = null;
+                                    }
+
+                                    if (fin != null) {
+                                        try {
+                                            final byte[] data = new byte[128];
+                                            final int length = fin.read(data);
+                                            final int x = fin.read();
+                                            final boolean moreData = (x >= 0);
+
+                                            final StringBuilder sb =
+                                                new StringBuilder();
+                                            for (int i = 0; i < length; i++) {
+                                                final byte b = data[i];
+                                                final char c = (char) b;
+                                                sb.append(c);
+                                            }
+                                            if (moreData) {
+                                                sb.append("...");
+                                            }
+                                            fileContents = sb.toString().trim();
+                                        } catch (final IOException e) {
+                                            fileContents = e.toString();
+                                        } finally {
+                                            try {
+                                                fin.close();
+                                            } catch (final IOException e) {
+                                                // oh well
+                                            }
+                                        }
+                                    }
+
+                                    final String contents =
+                                        context.getString(
+                                            R.string.debug_file_contents,
+                                            fileContents);
+                                    lines.add(contents);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            final String[] array = new String[lines.size()];
+            return lines.toArray(array);
         }
 
     }
