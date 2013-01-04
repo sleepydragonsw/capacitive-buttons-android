6ac976daa931b5e8bc5d5fe5f808309ad10aa72d
Denver Coneybeare <denver@sleepydragon.org>
2013-01-03 19:46:39 -0500

    Changed app name to simply "Capacitive Buttons"

--- src/org/sleepydragon/capbutnbrightness/SettingsActivity.java
+++ src/org/sleepydragon/capbutnbrightness/SettingsActivity.java
@@ -1,18 +1,18 @@
 /*
- * This file is part of Capacitive Button Brightness.
+ * This file is part of Capacitive Buttons.
  * 
- * Capacitive Button Brightness is free software: you can redistribute it
+ * Capacitive Buttons is free software: you can redistribute it
  * and/or modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
  * 
- * Capacitive Button Brightness is distributed in the hope that it will be
+ * Capacitive Buttons is distributed in the hope that it will be
  * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
  * Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with
- * Capacitive Button Brightness.  If not, see <http://www.gnu.org/licenses/>.
+ * Capacitive Buttons.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.sleepydragon.capbutnbrightness;
 
