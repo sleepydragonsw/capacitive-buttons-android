146c81815994862a84e8a9d26f70a1e89d3eaf0e
Denver Coneybeare <denver@sleepydragon.org>
2013-10-21 22:50:05 -0400

    MainActivity.java: remove unneeded line: import android.util.Log

--- CapButnBrightness/src/org/sleepydragon/capbutnbrightness/MainActivity.java
+++ CapButnBrightness/src/org/sleepydragon/capbutnbrightness/MainActivity.java
@@ -26,7 +26,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.Messenger;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
