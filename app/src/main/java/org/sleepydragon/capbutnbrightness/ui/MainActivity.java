67d25420295c72974631ee90d42ad5fd2715c02a
Denver Coneybeare <denver@sleepydragon.org>
2015-05-28 22:49:17 -0400

    MainActivity added, although it is blank

---
+++ app/src/main/java/org/sleepydragon/capbutnbrightness/ui/MainActivity.java
@@ -0,0 +1,28 @@
+package org.sleepydragon.capbutnbrightness.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import org.sleepydragon.capbutnbrightness.util.Logger;
+
+public class MainActivity extends FragmentActivity {
+
+    private final Logger mLogger;
+
+    public MainActivity() {
+        mLogger = new Logger("MainActivity");
+    }
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        mLogger.d("onCreate()");
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mLogger.d("onDestroy()");
+        super.onDestroy();
+    }
+
+}
